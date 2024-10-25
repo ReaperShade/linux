e6b28ae7374e9d2f2a89c7dfd5aeb752f9d9c999
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-25 10:11:52 -0400

    Add files via upload

---
+++ 01. System Information.c
@@ -0,0 +1,37 @@
+1. SYSTEM INFORMATION
+
+ 
+ 
+         - 'uname -a'                      //kernel name, hostname, the kernel release, kernel version, machine hardware name, and operating system.
+         - 'uname -r'                      //Display kernel release information
+         - 'uname -v'                      //OS version
+         - 'uname -m'                      //get Hardware name
+ 
+ 
+         - 'hostname'                      //print the name of the computer that we are logged into
+         - 'id'                            //Display the user and group ids of your current user.
+         - 'pwd'                           //Returns working directory name.
+         - 'ifconfig'                      //To view an address to a network interface and/or configure network interface parameters.
+         - 'ip'                            //Ip is a utility to show or manipulate routing, network devices, interfaces and tunnels.
+         - 'netstat'                       //Shows network status.
+         - 'ss'                            //Another utility to investigate socket
+         - 'ps'                            //Shows process status.
+         - 'env'                           //Prints environment or sets and executes command.
+         - 'lsblk'                         //Lists block devices.
+         - 'last'                          //Display the last users who have logged onto the system.
+         - 'last reboot'                   //Show system reboot history
+         - 'date'                          //Show the current date and time
+         - 'w'                             //Display who is online
+         - 'who'                           //Show who is logged into the system.
+         - 'whoami'             	       //Who you are logged in as
+ 
+
+
+         - 'history -c'                    //delete the history
+         - 'history -w'                    //save the historial.
+ 
+         - 'ssh [username]@[IP address]'   //to connect via SSH
+ 
+
+
+
